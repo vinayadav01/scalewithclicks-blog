5512c5e2bcd55485ec194308ff46a17fb6f3d937
Vinay yadav <vinayadav01@gmail.com>
2026-05-04 23:35:15 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,31 +1,7 @@
-"use client";
-
-import { useEffect, useState } from "react";
-
 export default function Navbar() {
-  const [scrolled, setScrolled] = useState(false);
-
-  useEffect(() => {
-  const handleScroll = () => {
-    const scrollTop = window.scrollY;
-    const docHeight = document.body.scrollHeight - window.innerHeight;
-    const progress = (scrollTop / docHeight) * 100;
-
-    const bar = document.getElementById("progress-bar");
-    if (bar) {
-      bar.style.width = progress + "%";
-    }
-
-    setScrolled(scrollTop > 50);
-  };
-
-  window.addEventListener("scroll", handleScroll);
-  return () => window.removeEventListener("scroll", handleScroll);
-}, []);
-  
   return (
-    <header className={`navbar ${scrolled ? "scrolled" : ""}`}>
-  
-</header>
+    <div style={{ padding: "20px", borderBottom: "1px solid #eee" }}>
+      <a href="/">Home</a> | <a href="/category/google-ads">Google Ads</a>
+    </div>
   );
 }
