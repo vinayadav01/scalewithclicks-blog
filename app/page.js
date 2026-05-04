597fa5f505092bd893c419f211fe7b71d1e27458
Vinay yadav <vinayadav01@gmail.com>
2026-05-04 23:38:09 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,10 +1,24 @@
+import Link from "next/link";
+import { getPosts } from "../lib/getPosts";
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
+  const posts = getPosts();
+
   return (
     <div style={{ padding: "40px" }}>
-      <h1>Blog Home</h1>
-      <a href="/blog/7-costly-google-ads-mistakes-that-are-wasting-your-budget">
-        Open Blog
-      </a>
+      <Navbar />
+
+      <h1>Blog</h1>
+
+      {posts.map((post) => (
+        <div key={post.slug} style={{ marginBottom: "20px" }}>
+          <Link href={`/blog/${post.slug}`}>
+            <h2>{post.title}</h2>
+          </Link>
+          <p>{post.description}</p>
+        </div>
+      ))}
     </div>
   );
 }
