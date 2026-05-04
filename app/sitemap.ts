5ecb8709d11aec3c27323b7c105ad71d21b0324f
Vinay yadav <vinayadav01@gmail.com>
2026-05-04 23:14:10 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,29 +1,18 @@
-import fs from "fs";
-import path from "path";
+import { getPosts } from "../lib/getPosts";
 
 export default function sitemap() {
   const baseUrl = "https://blog.scalewithclicks.com";
 
-  const blogDir = path.join(process.cwd(), "app/content/blog");
-
-  let posts: any[] = [];
-
-  if (fs.existsSync(blogDir)) {
-    const files = fs.readdirSync(blogDir);
-
-    posts = files
-      .filter((file) => file.endsWith(".md"))
-      .map((file) => ({
-        url: `${baseUrl}/blog/${file.replace(".md", "")}`,
-        lastModified: new Date(),
-      }));
-  }
+  const posts = getPosts();
 
   return [
     {
       url: baseUrl,
       lastModified: new Date(),
     },
-    ...posts,
+    ...posts.map((post) => ({
+      url: `${baseUrl}/blog/${post.slug}`,
+      lastModified: new Date(post.date),
+    })),
   ];
 }
