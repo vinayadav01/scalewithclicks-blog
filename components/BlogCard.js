ef46d21462c2568805b45e1e7ce37db3d5edb9ce
Vinay yadav <vinayadav01@gmail.com>
2026-05-05 11:18:35 +0530

    Update BlogCard.js

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -2,14 +2,13 @@ import Link from "next/link";
 
 export default function BlogCard({ post }) {
   return (
-    <div className="bg-white rounded-2xl shadow hover:shadow-lg transition overflow-hidden">
-
-      <img
-        src={post.image}
-        alt={post.title}
-        className="w-full h-48 object-cover"
-      />
-
+    <div className="relative h-48 w-full">
+  <img
+    src={post.image}
+    alt={post.title}
+    className="absolute inset-0 w-full h-full object-cover"
+  />
+</div>
       <div className="p-4">
         <span className="text-xs bg-purple-100 text-purple-600 px-2 py-1 rounded">
           {post.category}
@@ -28,7 +27,6 @@ export default function BlogCard({ post }) {
             Read More →
           </span>
         </Link>
-      </div>
-    </div>
+     </div>
   );
 }
