760bd08cc16c4b21e4ca5873c400f974a75f7d53
Vinay yadav <vinayadav01@gmail.com>
2026-05-06 17:43:18 +0530

    Create FadeIn.js

---
+++ components/FadeIn.js
@@ -0,0 +1,16 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+export default function FadeIn({ children, delay = 0 }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 30 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.5, delay }}
+    >
+      {children}
+    </motion.div>
+  );
+}
