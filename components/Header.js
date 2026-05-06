1bdda8afce2f286d8ce5492291019d4d331a1955
Vinay yadav <vinayadav01@gmail.com>
2026-05-06 18:04:49 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 backdrop-blur-xl bg-white/70 border-b border-black/5">
+   <header className="sticky top-0 z-50 glass">
       <div className="max-w-6xl mx-auto px-4 py-4 flex items-center justify-between">
 
         <Link href="/" className="font-semibold text-lg">
