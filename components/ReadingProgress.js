b6a0de5c89ec7f95279cbc4063829f37aa319683
Vinay yadav <vinayadav01@gmail.com>
2026-05-06 17:11:10 +0530

    Create ReadingProgress.js

---
+++ components/ReadingProgress.js
@@ -0,0 +1,28 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function ReadingProgress() {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const total =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+
+      const current = window.scrollY;
+      setProgress((current / total) * 100);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div
+      className="fixed top-0 left-0 h-[3px] bg-indigo-500 z-50"
+      style={{ width: `${progress}%` }}
+    />
+  );
+}
