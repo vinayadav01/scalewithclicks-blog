5c40bf19e5d89937479f94f2e5ab54917394c3a9
Vinay yadav <vinayadav01@gmail.com>
2026-05-10 17:25:57 +0530

    Update page.js

--- app/blog/page.js
+++ app/blog/page.js
@@ -33,7 +33,7 @@ export default function BlogPage() {
 
         {/* GRID */}
         <FadeIn>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 sm:gap-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
 
             {posts.map((post) => (
               <BlogCard key={post.slug} post={post} />
